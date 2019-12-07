4cf457b85773282e22db6e43818d09a9a55fe62a
Michael <michael.mhanna86@gmail.com>
2019-12-07 13:09:57 -0500

    working on popular drinks page, still unable to append on the HTML

--- assets/js/popular.js
+++ assets/js/popular.js
@@ -4,17 +4,16 @@ $(document).ready(function () {
     
      function init(){
           //event.preventDefault();
-          var searchInput = sessionStorage.getItem("search");
-          console.log(searchInput);
-          var queryURL = `https://www.thecocktaildb.com/api/json/v1/1/search.php?s=${searchInput}`;
-          console.log(queryURL);
-          $.ajax({
-               url: queryURL,
-               method: "GET"
-          }).then(function (response) {
-               console.log(response);
-               $("#searchDiv").empty();
-               for (var i = 0; i < response.drinks.length; i++) {
+          var idElement = ['11000', '11001', '11002', '11003', '11004', '11005', '11006', '11007', '11008', '11009'];
+     
+          for (var i = 0; i < idElement.length; i++) {
+               var queryURL = `https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i=${idElement[i]}`;
+               console.log(queryURL);
+               $.ajax({
+                    url: queryURL,
+                    method: "GET"
+               }).then(function (response) {
+                    console.log(response);
                     var a = $("<div>");
                     a.addClass("container-drink d-inline-block");
                     a.attr("data-name", response.drinks[i].idDrink);
@@ -30,10 +29,10 @@ $(document).ready(function () {
                     c.append(d);
                     a.append(c);
                     $("#searchDiv").append(a);
-               }
-          });
-     };  
-     
+               });
+          };
+     };
+
      $(document).on("click", ".container-drink", displayDrinkInfo);
 
 
@@ -41,10 +40,12 @@ $(document).ready(function () {
           var random = $("#randomBeverageCard");
           random.addClass("randomSelect");
      });
+     
      function displayDrinkInfo(){
           var selectedDrink = $(this).attr("data-name");
           alert(selectedDrink);
      }
 });
-
+  
+     
 
